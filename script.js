4544f8190efc25f8609745d5bb5ecfc4d44bb3e8
AnjaanaJ <anjanajanani6@gmail.com>
2026-06-01 14:36:21 +0530

    updates javascript file by adding the button feature

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function showMessage(){
 document.getElementById("message").innerHTML="CI/CD pipleline working!";
 }
+
+function showMyName(){
+    document.getElementById("name").innerHTML="Heyy!My Namwe Is Anjaana";
+}
